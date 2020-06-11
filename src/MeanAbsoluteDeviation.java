a5cbc02d018b0e0e191615e20249dde2310b70ef
suseelareshma25 <47037560+suseelareshma25@users.noreply.github.com>
2020-06-11 00:02:25 -0400

    MeanAbsoluteDeviation Added

---
+++ src/MeanAbsoluteDeviation.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * src.MeanAbsoluteDeviation class to compute the mean absolute deviation of the given input numbers
+ * @author Suseela Reshma Avireddy
+ *
+ */
+public class MeanAbsoluteDeviation {
+	
+	/**
+	 * This method returns the count of the input elements
+	 * @param inputList
+	 * @return (count)
+	 */
+	int getSize(ArrayList<Integer> inputList) {
+		int count=0;
+		for(Integer value: inputList) {
+
+			count++;
+		}
+		return count;
+	}
+	
+	/**
+	 * This method returns the absolute value for the input number
+	 * @param value
+	 * @return (value <= 0.0F) ? 0.0F - value : value
+	 */
+	double getAbsoluteValue(double value){
+		return (value <= 0.0F) ? 0.0F - value : value;
+
+	}
+	/**
+	 * This method computes and returns the mean absolute deviation of the given list of input numbers
+	 * @param list
+	 * @return (absSum / n)
+	 */
+	double meanAbsoluteDeviation(ArrayList<Integer> list) {
+		int n;
+		double arithmeticMean;
+		n=getSize(list);
+		ArithmeticMean am=new ArithmeticMean();
+		arithmeticMean=am.arithmeticMean(list);
+
+		double absSum = 0.0; 
+
+		for (int i = 0; i < n; i++) {
+			absSum = absSum + getAbsoluteValue(list.get(i))- arithmeticMean; 
+		}
+		return (absSum / n); 
+	} 
+
+}
